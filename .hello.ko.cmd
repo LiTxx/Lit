8f70672d3d5efcd2a85126ef087f5bee561ddb9a
LiTxx <676916894@qq.com>
2017-12-29 15:24:39 +0800

    请为您的变更输入提交说明。以 '#' 开始的行将被忽略，而一个空的提交

--- .hello.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/lit/drivers/hello.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/lit/drivers/hello.ko /home/lit/drivers/hello.o /home/lit/drivers/hello.mod.o ;  true
